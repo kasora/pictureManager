fefb63d4de88d647d76eaa4e595269ec0b4619d1
kasora <kasorasun@gmail.com>
2017-04-30 16:18:29 +0800

    fix bug.

--- server/api/picture.js
+++ server/api/picture.js
@@ -24,13 +24,6 @@ let checkInPicture = async (picture) => {
 }
 
 let uploadPicture = (req, res) => {
-  var form = formidable.IncomingForm({
-    encoding: 'utf-8',
-    uploadDir: config.todoPath,
-    keepExtensions: true,
-    maxFieldsSize: 2 * 4096 * 4096,
-  });
-
   fs.access(config.picturePath, function (err) {
     if (err) {
       fs.mkdirSync(config.picturePath);
@@ -42,37 +35,54 @@ let uploadPicture = (req, res) => {
     }
   });
 
-  var allFile = [];
-  form.on('file', function (filed, file) {
-    allFile.push([filed, file]);
+  var form = formidable.IncomingForm({
+    encoding: 'utf-8',
+    uploadDir: config.todoPath,
+    keepExtensions: true,
+    maxFieldsSize: 2 * 4096 * 4096,
+  });
+
+  let allFile = [];
+  form.on('file', function (field, file) {
+    allFile.push({ field, file });
   }).on('error', function (err) {
     console.error('上传失败：', err.message);
     res.status(500).send({ err: "upload error." });
-  }).parse(req, function (err, fields, files) {
+  }).parse(req, function (err, fields) {
     if (err) {
       res.status(500).send({ err: "system error." });
+      return;
     }
-    var errCount = 0;
-    var keys = Object.keys(files);
-    keys.forEach(async function (key) {
-      var filePath = files[key].path;
-      var fileExt = filePath.substring(filePath.lastIndexOf('.'));
-      if (('.jpg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
-        errCount += 1;
-      } else {
-        Object.assign(fields, req.query);
-        let picture = {
-          type: fileExt,
-          user: req.query._uid,
-          tags: fields.tags.split(','),
-        }
-        let fileName = await checkInPicture(picture) + fileExt;
-        let targetFile = path.join(config.picturePath, fileName);
 
-        fs.renameSync(filePath, targetFile);
-      }
+    new Promise((resolve, reject) => {
+      let last = allFile.length;
+      allFile.forEach(async function ({ field, file }) {
+        var filePath = file.path;
+        var fileExt = filePath.substring(filePath.lastIndexOf('.'));
+        if (('.jpg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
+          reject("unknown type.");
+        } else {
+          let picture = {
+            type: fileExt,
+            user: req.query._uid,
+            tags: fields[field].split(','),
+          }
+          let fileName = await checkInPicture(picture) + fileExt;
+          let targetFile = path.join(config.picturePath, fileName);
+
+          fs.renameSync(filePath, targetFile);
+          last--;
+        }
+        if (!last) {
+          resolve();
+        }
+      });
+    }).then(() => {
+      res.status(201).send({ status: "success" });
+    }, (err) => {
+      res.status(500).send({ err });
     });
-    res.status(201).send({ status: "success" });
+
   });
 }
 
